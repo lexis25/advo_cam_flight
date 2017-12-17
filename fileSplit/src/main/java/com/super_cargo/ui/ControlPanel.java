12a5d9e40f97b10c075cf2446d61e39ecfd11571
lexis25 <tokallex@gmail.com>
2017-12-17 21:37:20 +0200

    events

--- fileSplit/src/main/java/com/super_cargo/ui/ControlPanel.java
+++ fileSplit/src/main/java/com/super_cargo/ui/ControlPanel.java
@@ -1,6 +1,8 @@
 package com.super_cargo.ui;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.scene.Scene;
@@ -8,12 +10,22 @@ import javafx.scene.control.*;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Paint;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class ControlPanel extends Application {
+    private ToggleGroup groupAddress;
+    private RadioButton def;
+    private RadioButton newURL;
+    private TextField textURL;
+    private ComboBox comboBoxFrom;
+    private ComboBox comboBoxBefore;
+    private RadioButton txt;
+    private RadioButton excel;
+    private Button cancel;
+    private Button start;
+    private RadioButton saveDefPath;
+    private RadioButton saveNewPath;
+
 
 
     public void start(Stage primaryStage) throws Exception {
@@ -24,12 +36,14 @@ public class ControlPanel extends Application {
         pane.getChildren().add(createFiles());
         pane.getChildren().add(createButtons());
 
-        Scene scene = new Scene(pane, 500, 550);
+        Scene scene = new Scene(pane, 400, 520);
 
         primaryStage.setTitle("Airport VRParser");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
+        Controller controller = new Controller();
+        controller.setEvents();
 
     }
 
@@ -37,130 +51,129 @@ public class ControlPanel extends Application {
         launch(args);
     }
 
-    private static HBox createURLaddress() {
+    private  VBox createURLaddress() {
         VBox addressVBox = new VBox();
         addressVBox.setPadding(new Insets(25));
 
-        ToggleGroup groupAddress = new ToggleGroup();
-        RadioButton def = new RadioButton();
-        def.setText("default");
+        groupAddress = new ToggleGroup();
+        def = new RadioButton("default");
         def.setPadding(new Insets(5));
         def.setSelected(true);
         def.setToggleGroup(groupAddress);
+        def.setUserData("default");
 
-        RadioButton newURL = new RadioButton();
-        newURL.setText("new URL");
+        newURL = new RadioButton("new URL");
         newURL.setToggleGroup(groupAddress);
         newURL.setPadding(new Insets(5));
+        newURL.setUserData("new URL");
 
-        TextField textURL = new TextField();
+        textURL = new TextField();
         textURL.setPromptText("www.address.com");
         textURL.setPadding(new Insets(5));
 
-        Line line = new Line();
-        line.setStartX(138);
-        line.setStartY(198);
-        line.setEndX(500);
-        line.setEndY(198);
+        addressVBox.getChildren().addAll(def, newURL, textURL);
 
-        addressVBox.getChildren().addAll(def, newURL, textURL, line);
-
-        Rectangle rectangleAddress = new Rectangle(89, 138, Paint.valueOf("#0c59cf"));
-
-        HBox addressHBox = new HBox();
-        addressHBox.getChildren().addAll(rectangleAddress, addressVBox);
-
-        return addressHBox;
+        return addressVBox;
 
     }
 
-    private static HBox createDateBox() {
+    private  HBox createDateBox() {
         VBox dateFromVBox = new VBox();
-        dateFromVBox.setPadding(new Insets(25));
-        VBox dateBeforeVBox = new VBox();
-        dateBeforeVBox.setPadding(new Insets(25));
+        dateFromVBox.setPadding(new Insets(5));
 
-        Label labelDate = new Label("Choose a date interval");
+        VBox dateBeforeVBox = new VBox();
+        dateBeforeVBox.setPadding(new Insets(5));
+
+        VBox labelBox = new VBox();
+        labelBox.setPadding(new Insets(5));
+
+        HBox dateContHBox = new HBox();
+        dateContHBox.setPadding(new Insets(20));
+
+        Label labelDate = new Label("Choose a date interval" + '\n' + " parse data");
 
         Label from = new Label("from");
         from.setPadding(new Insets(5));
-        ComboBox comboBoxFrom = new ComboBox();
+        comboBoxFrom = new ComboBox();
         comboBoxFrom.getItems().addAll("yesterday", "today", "tomorrow");
 
         Label before = new Label("before");
         before.setPadding(new Insets(5));
 
-        ComboBox comboBoxBefore = new ComboBox();
+        comboBoxBefore = new ComboBox();
         comboBoxBefore.getItems().addAll("yesterday", "today", "tomorrow");
 
+        dateFromVBox.getChildren().addAll(from, comboBoxFrom);
+        dateBeforeVBox.getChildren().addAll(before,comboBoxBefore);
+        labelBox.getChildren().addAll(labelDate);
 
-        Rectangle rectangleDate = new Rectangle(89, 108, Paint.valueOf("#3d8af7"));
-
-        dateFromVBox.getChildren().addAll(labelDate, from, before, comboBoxFrom);
-        dateBeforeVBox.getChildren().addAll(before, comboBoxBefore);
+        dateContHBox.getChildren().addAll(labelBox,dateFromVBox, dateBeforeVBox);
         HBox dateHBox = new HBox();
-        dateHBox.getChildren().addAll(rectangleDate, dateFromVBox, dateBeforeVBox);
+        dateHBox.getChildren().addAll(dateContHBox);
 
         return dateHBox;
     }
 
-    private static HBox createFiles(){
+    private VBox createFiles(){
 
         VBox filesVBox = new VBox();
-        filesVBox.setPadding(new Insets(25));
+        filesVBox.setPadding(new Insets(20));
 
         ToggleGroup groupFiles = new ToggleGroup();
-        RadioButton txt = new RadioButton();
-        txt.setText("Create txt file schedule");
+        txt = new RadioButton("Create txt file schedule");
         txt.setSelected(true);
         txt.setPadding(new Insets(5));
 
-        RadioButton excel = new RadioButton();
-        excel.setText("Create excel file schedule");
+        excel = new RadioButton("Create excel file schedule");
         excel.setPadding(new Insets(5));
 
-        Rectangle rectangleFiles = new Rectangle(89,78, Paint.valueOf("#75a9f9"));
-
         filesVBox.getChildren().addAll(txt,excel);
-        HBox filesHBox = new HBox();
-        filesHBox.getChildren().addAll(rectangleFiles,filesVBox);
 
-        return filesHBox;
+        return filesVBox;
     }
 
-    private static HBox createButtons(){
+    private VBox createButtons(){
         HBox buttonHBox = new HBox(10);
         VBox buttonVBox = new VBox();
 
         buttonHBox.setPadding(new Insets(25));
         buttonVBox.setPadding(new Insets(25));
 
-        Button cancel = new Button("cancel");
-        Button start = new Button(" start ");
+        cancel = new Button("cancel");
+        start = new Button(" start ");
 
         Label labelSaveFile = new Label("set default path directory");
 
         ToggleGroup group = new ToggleGroup();
-        RadioButton saveDefPath = new RadioButton();
-        saveDefPath.setText("default");
+        saveDefPath = new RadioButton("default");
         saveDefPath.setPadding(new Insets(5));
         saveDefPath.setSelected(true);
         saveDefPath.setToggleGroup(group);
 
-        RadioButton saveNewPath = new RadioButton();
-        saveNewPath.setText("new path");
+        saveNewPath = new RadioButton("new path");
         saveNewPath.setPadding(new Insets(5));
         saveNewPath.setToggleGroup(group);
-
 
         buttonHBox.getChildren().addAll(cancel,start);
         buttonVBox.getChildren().addAll(labelSaveFile,saveDefPath,saveNewPath,buttonHBox);
 
-        Rectangle rectangleButtons = new Rectangle(89,48,Paint.valueOf("#a8c6fa"));
-
-        HBox buttonsContainer = new HBox(0);
-        buttonsContainer.getChildren().addAll(rectangleButtons,buttonVBox);
-        return buttonsContainer;
+        return buttonVBox;
     }
 
+    public class Controller {
+        private String addressURL = "https://hrk.aero/table/ajax_tablo_new.php?lang=ru&full=1&first=1";
+        private String path;
+
+        private void setEvents(){
+            groupAddress.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+                public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
+                    if(groupAddress.getSelectedToggle().getUserData().equals("default")){
+                        addressURL = "https://hrk.aero/table/ajax_tablo_new.php?lang=ru&full=1&first=1";
+                    }else{
+                        addressURL = textURL.getText();
+                    }
+                }
+            });
+        }
+    }
 }
